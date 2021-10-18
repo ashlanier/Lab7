525b6832d4893ce16a8d3c799cfa08bacb246ab1
ashlanier <90577255+ashlanier@users.noreply.github.com>
2021-10-17 21:38:15 -0400

    Update netmikoLab.py

--- netmikoLab.py
+++ netmikoLab.py
@@ -1,1 +1,14 @@
-print("Test")
+from netmiko import ConnectHandler
+from getpass import getpass
+
+USERNAME = input("Enter your SSH username: ")
+PASSWORD = getpass("Enter your SSH password: ")
+
+device = {
+    'ip': '192.168.0.0',
+    'username': USERNAME,
+    'password': PASSWORD,
+    'device_type': 'cisco_ios'
+}
+
+c = ConnectHandler(**device)
